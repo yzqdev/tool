a74343209fc7dee457705a63fc9337646d9613c5
yzqdev <yzqdev@outlook.com>
2026-02-19 23:15:43 +0800

    update deps

--- src/actions/cmds.ts
+++ src/actions/cmds.ts
@@ -25,11 +25,11 @@ export function flutterCommands() {
 export function nameCommands(){
   const genshinNames:string[]=['jean','amber','lisa','kaeya','barbara','diluc','razor','venti','klee','noelle','mona','diona','eula','aloy','mika','ayaka','kokomi','sayu','thoma','gorou','kirara','mizuki','collei','dori','nilou','nahida','layla','lyney','lynette','furina','navia']
   const starrailNames: string[] = ['himko','welt','blade','firefly','topaz','jade','misha','robin','ratio','swan','sparkle','arlan','asta','herta','ruanmei','gepard','bronya','seele','clara','sampo','pela','natasha'];
-  const waveNames: string[] = ['phoebe','brant','changli','lumi','carlotta','roccia','camellya','shorekeeper','encore','verina'];
+  const waveNames: string[] = ['phoebe','brant','changli','lumi','carlotta','roccia','camellya','shorekeeper','encore','verina','lynae','chisa','ameath','luno','augusta','lupa','cartethyia','mornye','Phrolova'];
   const otherNames: string[] = ['ironman','strange','captain','nick','stark','tony','hulk','bruce','thor','marvel','peter','parker','anna','elsa','sven','olaf','kristen'];
   const all=[...genshinNames,...starrailNames,...waveNames,...otherNames]
   const len=all.length
   const rand = Math.floor(Math.random() * len) + 1;
-  
+
   console.log(all[rand])
 }
